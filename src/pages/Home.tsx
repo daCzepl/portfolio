74185cd5c54b52599ee90ecce7c08e192aabd828
Stefan <20190050@students.htl-perg.ac.at>
2023-07-07 12:26:24 +0200

    changed title displaying

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,12 +2,16 @@ import { WritingAnimationText } from "../components/writing-animated-text";
 
 export const Home = () => {
   return (
-    <div className="text-white flex h-full items-center px-12">
-      <WritingAnimationText styling="text-7xl">
-        Hi, <br />
-        I'm Stefan <br />
-        software developer
-      </WritingAnimationText>
+    <div className="text-white flex h-full justify-center items-center px-12 flex-col">
+      <div className="text-center mb-8">
+        <h1 className="text-9xl">Stefan Czepl</h1>
+      </div>
+      <div className="text-center mb-80">
+        <WritingAnimationText styling="text-3xl">
+          Software Engineer & App Developer.
+        </WritingAnimationText>
+      </div>
     </div>
   );
 };
+
