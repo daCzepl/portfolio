8106b1aec3e448b48f6dc1fcbd675b211ec935fd
Stefan <20190050@students.htl-perg.ac.at>
2023-09-15 08:35:21 +0200

    added text to about me page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,20 +1,38 @@
+import placeHolder from "../assets/placeholder_image.jpg";
+
 export const About = () => {
   return (
     <div className="bg-neutral-800 h-screen" id="about">
       <div className="text-white h-full items-center flex px-12 flex-col">
-        <h1 className="text-3xl md:text-5xl lg:text-7xl text-center mb-8">Im Stefan.</h1>
-        <p className="leading-normal">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
-          lacinia massa nec mauris condimentum, ac aliquet est fermentum.
-          Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
-          posuere cubilia curae; Quisque sed pellentesque felis, vitae luctus
-          nibh. Pellentesque habitant morbi tristique senectus et netus et
-          malesuada fames ac turpis egestas. Sed auctor, dui in iaculis laoreet,
-          enim orci sodales sem, eget blandit mauris nisl non augue. Curabitur
-          vulputate, dui ac facilisis iaculis, turpis lorem dapibus ipsum, a
-          accumsan dolor ipsum at diam. Nunc dictum lacus nec enim lacinia
-          blandit.
-        </p>
+        <h1 className="text-3xl md:text-5xl lg:text-7xl text-center mb-28 font-bold">
+          I'm Stefan.
+        </h1>
+        <div className="grid grid-cols-2">
+          <div className="w-96 ml-28">
+            <img src={placeHolder} alt="Image" />
+          </div>
+          <div>
+            <div className="mb-10">
+              <p className="leading-normal text-2xl md:text-4xl lg:text-5xl max-w-2xl">
+                I'm a Software Engineer and App Developer from St.Valentin,
+                Austria.
+              </p>
+            </div>
+            <div className="leading-relaxed text-lg md:text-xl lg:text-2xl max-w-2xl">
+              <div className="mb-10">
+                <p>
+                  I'm currently attending the 5th form of HTL Perg for Software
+                  Engineering. I have worked on various projects which you can
+                  find under the Projects section.
+                </p>
+              </div>
+              <p>
+                I specialize in building backend solutions using Java, C# and
+                Python.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
