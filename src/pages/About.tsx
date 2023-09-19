03d75e78cb74bb96ff01423ff88993aa42093ca4
Stefan <20190050@students.htl-perg.ac.at>
2023-09-19 13:37:23 +0200

    adjusted mobile responsiveness

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,23 +3,27 @@ import placeHolder from "../assets/placeholder_image.jpg";
 export const About = () => {
   return (
     <div className="bg-neutral-800 h-screen pt-10" id="about">
-      <div className="text-white h-full items-center flex px-12 flex-col">
-        <h1 className="text-3xl md:text-5xl lg:text-7xl text-center mb-28 font-bold">
+      <div className="text-white h-full items-center flex px-4 md:px-12 flex-col">
+        <h1 className="text-3xl md:text-5xl lg:text-7xl text-center mb-10 md:mb-28 font-bold">
           I'm Stefan.
         </h1>
-        <div className="grid grid-cols-2">
-          <div className="w-96 ml-28 overflow-hidden rounded-tl-full rounded-tr-full h-auto">
-            <img src={placeHolder} alt="Image" className="w-full h-auto" />
+        <div className="sm:grid sm:grid-cols-1 md:grid-cols-2">
+          <div className="flex justify-center w-full md:w-96 ml-2 md:ml-28 h-auto mx-auto md:mx-0 mb-10 md:mb-0">
+            <img
+              src={placeHolder}
+              alt="Image"
+              className="h-auto rounded-tl-full rounded-tr-full"
+            />
           </div>
-          <div>
-            <div className="mb-10">
-              <p className="leading-normal text-2xl md:text-4xl lg:text-5xl max-w-2xl">
+          <div className="mt-5 md:mt-0">
+            <div className="mb-6 md:mb-10">
+              <p className="leading-normal text-xl md:text-4xl lg:text-5xl max-w-full md:max-w-2xl">
                 I'm a Software Engineer and App Developer from St.Valentin,
                 Austria.
               </p>
             </div>
-            <div className="leading-relaxed text-lg md:text-xl lg:text-2xl max-w-3xl font-thin">
-              <div className="mb-10">
+            <div className="leading-relaxed text-lg md:text-xl lg:text-2xl max-w-full md:max-w-3xl font-thin">
+              <div className="mb-6 md:mb-10">
                 <p>
                   I'm currently attending the 5th form of HTL Perg for Software
                   Engineering. I have worked on various projects which you can
